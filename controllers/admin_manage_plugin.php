ed515169256979228a7386e77f128e041dc2f9d4
git-uname <a.french@mail.utoronto.ca>
2016-08-23 18:52:17 -0400

    Fixed some function names

--- controllers/admin_manage_plugin.php
+++ controllers/admin_manage_plugin.php
@@ -3,7 +3,7 @@ class AdminManagePlugin extends AppController {
     private function init() {
         $this->parent->requireLogin();
 
-        $this->uses(array('UGCCSetup.UGCCSetupSettings'));
+        $this->uses(array('Ugccsetup.UgccsetupSettings'));
         
         $this->parent->structure->set('page_title', 'UGCC Setup Settings', true);
 
@@ -13,10 +13,10 @@ class AdminManagePlugin extends AppController {
     public function index() {
         $this->init();
 
-        $vars = (object) $this->UGCCSetupSettings->getSettings($this->parent->company_id);
+        $vars = (object) $this->UgccsetupSettings->getSettings($this->parent->company_id);
 
         if (!empty($this->post)) {
-            $this->UGCCSetupSettings->setSettings($this->parent->company_id, $this->post);
+            $this->UgccsetupSettings->setSettings($this->parent->company_id, $this->post);
         }
 
         $vars = (object) $this->post;

1d0c62f3ce5b9c76c4f8fcd7003c0fdd028428c1
git-uname <a.french@mail.utoronto.ca>
2016-08-24 12:52:11 -0400

    Initial partial setup implementation

--- ugccsetup_plugin.php
+++ ugccsetup_plugin.php
@@ -19,13 +19,33 @@ class UgccsetupPlugin extends Plugin {
 	}
 
 	private function handleCInvoice($event) {
-		$params = $events->getParams();
-		$invoice_id = $params['invoice_id'];
+                Loader::loadModels($this, array("Invoices");
+                Loader::loadModels($this, array("Clients");
 
+                $params = $events->getParams();
+		$invoice_id = $params["invoice_id"];
+                $invoice = $this->Invoices->get($invoice_id);
+                if ($invoice["paid"] == 0)
+                        return
+                if ($invoice["total"] < $invoice["paid"]) {
+                        warn(false, "Invoice not fully paid. Client paid " . $invoice['paid'] . "but total was " . $invoice['total']);
+                }
+
+                $username = $this->Clients->get($invoice["client_id"];
+                if ($username == "")
+                        $username = preg_replace("/[^A-Za-z0-9]/", "", $invoice["email"]);
+                if ($username == "") {
+                        return warn(true, "Invalid username")
+                }
+                
 	}
 
-	public function makeServer($game, $slots, $username) {
+	public function makeServer($service_id, $username) {
+        $services = JSON.parse('setup.json');
+    }
 
-	}
+    public function warn($fatal, $warning_msg) {
+
+    }
 }
 ?>
